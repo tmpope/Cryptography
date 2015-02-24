bf5c6cb4e276c47a791f7790af36fefdc9212537
tmpope <legoruthead@gmail.com>
2015-02-24 14:01:22 -0700

    Added a console application to use as scratch paper

---
+++ CryptoMethods/ConsoleApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoMethods;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = 2387556325;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(CryptoMethods.ModularArithmatic.FastExponentiate(3, a, 38200901201));
+                a *= 2;
+            }
+            Console.WriteLine(CryptoMethods.ModularArithmatic.EuclidianAlgorithm(208, 485));
+            long b = 3599;
+            Console.WriteLine(CryptoMethods.ModularArithmatic.FastExponentiate(2, (b - 1) / 2, b));
+            
+            Console.Read();
+        }
+    }
+}
